44bad5d2b30c492a0c99f58433f984f559d7973e
Ana Carolina <carolbd1107@gmail.com>
2023-01-12 17:47:23 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/anacarolina/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anacarolina.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
