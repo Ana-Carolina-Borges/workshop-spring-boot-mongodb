a040be98339957aa4c425aab154d1848d4e5177f
Ana Carolina <carolbd1107@gmail.com>
2023-01-18 11:20:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/anacarolina/workshopmongo/services/UserService.java
+++ src/main/java/com/anacarolina/workshopmongo/services/UserService.java
@@ -5,7 +5,9 @@ import com.anacarolina.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ObjectStreamException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,12 @@ public class UserService {
     public List<User> findAll() {
         return userRepository.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new RuntimeException("Objeto não encontrado"));
+
+    }
+
+
 }
