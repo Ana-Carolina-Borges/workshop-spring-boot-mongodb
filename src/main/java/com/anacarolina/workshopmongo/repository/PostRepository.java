cd48fb95cb32e53755f9093fc20dd8036400e2a4
Ana Carolina <carolbd1107@gmail.com>
2023-01-24 20:32:05 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/anacarolina/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.anacarolina.workshopmongo.repository;
+
+import com.anacarolina.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
