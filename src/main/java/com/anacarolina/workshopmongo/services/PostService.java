37a8dcf39f838877647d5d913c9ccfa30091cd52
Ana Carolina <carolbd1107@gmail.com>
2023-01-31 14:11:28 -0300

    Consulta com vários critérios

--- src/main/java/com/anacarolina/workshopmongo/services/PostService.java
+++ src/main/java/com/anacarolina/workshopmongo/services/PostService.java
@@ -6,21 +6,26 @@ import com.anacarolina.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PostService {
-
     @Autowired
-    private PostRepository repo;
+    private PostRepository postRepository;
 
     public Post findById(String id) {
-        Optional<Post> obj = repo.findById(id);
+        Optional<Post> obj = postRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public List<Post> findByTitle(String text){
-        return repo.searchTitle(text);
+    public List<Post> findByTitle(String text) {
+        return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
